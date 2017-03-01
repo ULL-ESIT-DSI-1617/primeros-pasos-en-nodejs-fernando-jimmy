0c0587e061162c5f65fc931a38c16ea5c49bd4e7
Alex Jimmy <alu0100728170@ull.edu.es>
2017-03-01 01:25:25 +0000

    Tarea deploy en gulpfile implementada

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,7 @@ const exec = require('child_process').exec
 gulp.task('build', ()=> {
 	exec('node ./scripts/gitbook-build.js');
 });
+
+gulp.task('deploy', ()=> {
+	exec('node ./scripts/deploy.js');
+})
